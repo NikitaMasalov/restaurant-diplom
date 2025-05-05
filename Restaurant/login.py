0ee014ab765a334c4be83a4f97de67d0b5b527ee
NikitaMasalov <118043414+NikitaMasalov@users.noreply.github.com>
2025-05-05 11:02:04 +0300

    1.2

--- Restaurant/login.py
+++ Restaurant/login.py
@@ -1,20 +1,39 @@
-
 from mybd import get_connection
+import tkinter as tk
+from tkinter import  messagebox
+import  uuid
+import mysql.connector
+from datetime import datetime, timedelta
 
-def test_connection():
-    conn = get_connection()
-    if conn is not None:
-        try:
-            cursor = conn.cursor()
-            cursor.execute("SELECT DATABASE();")
-            db_name = cursor.fetchone()
-            print(f"Успешное подключение к базе данных: {db_name[0]}")
-        except Exception as e:
-            print("Ошибка при выполнении запроса:", e)
-        finally:
-            conn.close()
-    else:
-        print("Подключение не удалось.")
 
-if __name__ == "__main__":
-    test_connection()
+def generate_session():
+    code = str(uuid.uuid4())[:4]
+    created_at = datetime.now()
+    expires_at = created_at + timedelta(minutes=30)
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO kitchen_codes (code, created_at, expires_at, used) VALUES (%s, %s, %s, %s)", (code, created_at, expires_at, 0))
+        conn.commit()
+        cursor.close()
+        return code
+    except mysql.connector.Error as err:
+        messagebox.showerror("Ошибка БД", f"Ошибка подключения: {err}")
+        return None
+
+def on_tracker_click():
+    code = generate_session()
+    if code:
+        messagebox.showinfo("Трекер", f'Код сеанса: {code}')
+
+root = tk.Tk()
+root.title("Вход в систему")
+root.geometry("300x200")
+
+label = tk.Label(root, text="Войдите в систему")
+label.pack(pady=20)
+
+btn_traker= tk.Button(root, text="Вход в трекер", command=on_tracker_click)
+btn_traker.pack(pady=5)
+
+root.mainloop()
